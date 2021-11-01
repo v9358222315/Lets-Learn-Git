f334b996a8de6f5c976084c06b9f3827c5c72326
Vijendra Saini <v9358222315@gmail.com>
2021-11-01 13:13:43 +0530

    i made change in print.js

--- print.js
+++ print.js
@@ -1,4 +1,6 @@
 console.log("The auther is from B-CLUB");
 console.log("The name of this guy is :-");
 console.log("Vijendra Saini");
+console.log("Hey dude you have made your first git repo and it is sent on git hub.");
+//End of Code 
 
